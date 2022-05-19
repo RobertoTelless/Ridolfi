b880e2e7125823b5662985d0c8d8ae66365dbdd3
Roberto Telles <robertotelles00@gmail.com>
2022-05-19 10:52:45 -0300

    Upload 19/05/2022 1

---
+++ Ridolfi_Presentation/Controllers/ConfiguracaoController.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ApplicationServices.Interfaces;
+using EntitiesServices.Model;
+using System.Globalization;
+using SMS_Presentation.App_Start;
+using EntitiesServices.Work_Classes;
+using AutoMapper;
+using ERP_CRM_Solution.ViewModels;
+using System.IO;
+using Correios;
+using Canducci.Zip;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using EntitiesServices.Attributes;
+using OfficeOpenXml.Table;
+using EntitiesServices.WorkClasses;
+using System.Threading.Tasks;
+
+namespace ERP_CRM_Solution.Controllers
+{
+    public class ConfiguracaoController : Controller
+    {
+        private readonly IConfiguracaoAppService baseApp;
+        private readonly ILogAppService logApp;
+        private readonly IUsuarioAppService usuApp;
+
+        private String msg;
+        private Exception exception;
+        CONFIGURACAO objeto = new CONFIGURACAO();
+        CONFIGURACAO objetoAntes = new CONFIGURACAO();
+        List<CONFIGURACAO> listaMaster = new List<CONFIGURACAO>();
+        String extensao;
+
+        public ConfiguracaoController(IConfiguracaoAppService baseApps, ILogAppService logApps, IUsuarioAppService usuApps)
+        {
+            baseApp = baseApps;
+            logApp = logApps;
+            usuApp = usuApps;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            if ((String)Session["Ativa"] == null)
+            {
+                return RedirectToAction("Login", "ControleAcesso");
+            }
+            return View();
+        }
+
+        public ActionResult Voltar()
+        {
+
+            return RedirectToAction("CarregarBase", "BaseAdmin");
+        }
+
+        [HttpPost]
+        public JsonResult GetConfiguracao()
+        {
+            var config = baseApp.GetItemById(2);
+            var serialConfig = new CONFIGURACAO
+            {
+                CONF_CD_ID = config.CONF_CD_ID,
+                CONF_NR_FALHAS_DIA = config.CONF_NR_FALHAS_DIA,
+                CONF_NM_HOST_SMTP = config.CONF_NM_HOST_SMTP,
+                CONF_NM_PORTA_SMTP = config.CONF_NM_PORTA_SMTP,
+                CONF_NM_EMAIL_EMISSOO = config.CONF_NM_EMAIL_EMISSOO,
+                CONF_NM_SENHA_EMISSOR = config.CONF_NM_SENHA_EMISSOR,
+                CONF_NR_REFRESH_DASH = config.CONF_NR_REFRESH_DASH,
+                CONF_NM_ARQUIVO_ALARME = config.CONF_NM_ARQUIVO_ALARME,
+                CONF_NR_REFRESH_NOTIFICACAO = config.CONF_NR_REFRESH_NOTIFICACAO,
+                CONF_SG_LOGIN_SMS = config.CONF_SG_LOGIN_SMS,
+                CONF_SG_SENHA_SMS = config.CONF_SG_SENHA_SMS,
+            };
+
+            return Json(serialConfig);
+        }
+
+        [HttpGet]
+        public ActionResult MontarTelaConfiguracao()
+        {
+            // Verifica se tem usuario logado
+            USUARIO usuario = new USUARIO();
+            if ((String)Session["Ativa"] == null)
+            {
+                return RedirectToAction("Login", "ControleAcesso");
+            }
+            if ((USUARIO)Session["UserCredentials"] != null)
+            {
+                usuario = (USUARIO)Session["UserCredentials"];
+
+                // Verfifica permissão
+                if (usuario.PERFIL.PERF_SG_SIGLA != "ADM")
+                {
+                    Session["MensPermissao"] = 2;
+                    return RedirectToAction("CarregarBase", "BaseAdmin");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "ControleAcesso");
+            }
+            Int32 idAss = (Int32)Session["IdAssinante"];
+
+
+            // Carrega listas
+            objeto = baseApp.GetItemById(idAss);
+            Session["Configuracao"] = objeto;
+
+            ViewBag.Listas = (CONFIGURACAO)Session["Configuracao"];
+            ViewBag.Title = "Configuracao";
+            var listaGrid = new List<SelectListItem>();
+            listaGrid.Add(new SelectListItem() { Text = "10", Value = "10" });
+            listaGrid.Add(new SelectListItem() { Text = "25", Value = "25" });
+            listaGrid.Add(new SelectListItem() { Text = "50", Value = "50" });
+            listaGrid.Add(new SelectListItem() { Text = "100", Value = "100" });
+            ViewBag.ListaGrid = new SelectList(listaGrid, "Value", "Text");
+
+            // Indicadores
+
+            // Mensagem
+
+            // Abre view
+            Session["MensConfiguracao"] = 0;
+            objetoAntes = objeto;
+            if (objeto.CONF_NR_FALHAS_DIA == null)
+            {
+                objeto.CONF_NR_FALHAS_DIA = 3;
+            }
+            Session["Configuracao"] = objeto;
+            Session["IdConf"] = 1;
+            ConfiguracaoViewModel vm = Mapper.Map<CONFIGURACAO, ConfiguracaoViewModel>(objeto);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult MontarTelaConfiguracao(ConfiguracaoViewModel vm)
+        {
+            if ((String)Session["Ativa"] == null)
+            {
+                return RedirectToAction("Login", "ControleAcesso");
+            }
+            Int32 idAss = (Int32)Session["IdAssinante"];
+            var listaGrid = new List<SelectListItem>();
+            listaGrid.Add(new SelectListItem() { Text = "10", Value = "10" });
+            listaGrid.Add(new SelectListItem() { Text = "25", Value = "25" });
+            listaGrid.Add(new SelectListItem() { Text = "50", Value = "50" });
+            listaGrid.Add(new SelectListItem() { Text = "100", Value = "100" });
+            ViewBag.ListaGrid = new SelectList(listaGrid, "Value", "Text");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Executa a operação
+                    USUARIO usuarioLogado = (USUARIO)Session["UserCredentials"];
+                    CONFIGURACAO item = Mapper.Map<ConfiguracaoViewModel, CONFIGURACAO>(vm);
+                    Int32 volta = baseApp.ValidateEdit(item, objetoAntes, usuarioLogado);
+
+                    // Sucesso
+                    objeto = new CONFIGURACAO();
+                    Session["ListaConfiguracao"] = null;
+                    Session["Configuracao"] = null;
+                    Session["MensConfiguracao"] = 0;
+                    return RedirectToAction("MontarTelaConfiguracao");
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = ex.Message;
+                    return View(vm);
+                }
+            }
+            else
+            {
+                return View(vm);
+            }
+        }
+
+        public ActionResult VoltarBaseConfiguracao()
+        {
+            if ((String)Session["Ativa"] == null)
+            {
+                return RedirectToAction("Login", "ControleAcesso");
+            }
+            return RedirectToAction("MontarTelaConfiguracao");
+        }
+
+        [HttpGet]
+        public ActionResult EditarConfiguracao(Int32 id)
+        {
+            // Prepara view
+            if ((String)Session["Ativa"] == null)
+            {
+                return RedirectToAction("Login", "ControleAcesso");
+            }
+            Int32 idAss = (Int32)Session["IdAssinante"];
+
+            CONFIGURACAO item = baseApp.GetItemById(id);
+            objetoAntes = item;
+            Session["Configuracao"] = item;
+            Session["IdVolta"] = id;
+            ConfiguracaoViewModel vm = Mapper.Map<CONFIGURACAO, ConfiguracaoViewModel>(item);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult EditarConfiguracao(ConfiguracaoViewModel vm)
+        {
+            if ((String)Session["Ativa"] == null)
+            {
+                return RedirectToAction("Login", "ControleAcesso");
+            }
+            Int32 idAss = (Int32)Session["IdAssinante"];
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Executa a operação
+                    USUARIO usuarioLogado = (USUARIO)Session["UserCredentials"];
+                    CONFIGURACAO item = Mapper.Map<ConfiguracaoViewModel, CONFIGURACAO>(vm);
+                    Int32 volta = baseApp.ValidateEdit(item, objetoAntes, usuarioLogado);
+
+                    // Sucesso
+                    objeto = new CONFIGURACAO();
+                    Session["ListaConfiguracao"] = null;
+                    Session["MensConfiguracao"] = 0;
+                    return RedirectToAction("MontarTelaConfiguracao");
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = ex.Message;
+                    return View(vm);
+                }
+            }
+            else
+            {
+                return View(vm);
+            }
+        }
+
+
+    }
+}
